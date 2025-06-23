e25c2607bbeddb6adcbb5910b9147fa2b8b7ed50
DerwinPerez <derwin.perez327@std.uni.edu.ni>
2025-06-23 14:22:52 -0600

    cambio saludo

--- Desktop/miproyecto/Miproyecto.java
+++ Desktop/miproyecto/Miproyecto.java
@@ -1,10 +1,12 @@
 public class Miproyecto{
     public static void main(String[] args) {
         String nombre= "Estudiante";
-        int a=3;
-        int b=5;
-        int suma =a+b;
+        int a=4;
+        int b=10;
+        int mult =a*b;
         System.out.println("hola "+ nombre);
-        System.out.println("la suma de "+a+" + "+b+" es "+suma);
+        System.out.println("la suma de "+a+" + "+b+" es "+mult);
+        System.out.println("DERWIN WILSON PEREZ ESTRADA");
+        System.out.println("2025-1559U");
     }
 }
