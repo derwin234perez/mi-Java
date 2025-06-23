a2d873e90500e32c913575052ab024ab47b0543f
DerwinPerez <derwin.perez327@std.uni.edu.ni>
2025-06-22 20:11:36 -0600

    primer commit: archivo Miproyecto.java creado

---
+++ Desktop/miproyecto/Miproyecto.java
@@ -0,0 +1,10 @@
+public class Miproyecto{
+    public static void main(String[] args) {
+        String nombre= "Estudiante";
+        int a=3;
+        int b=5;
+        int suma =a+b;
+        System.out.println("hola "+ nombre);
+        System.out.println("la suma de "+a+" + "+b+" es "+suma);
+    }
+}
